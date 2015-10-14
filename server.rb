71e7f98fdcdada8191375284cb90fbd9f59bbd70
Adrian Knapp <adrianknapp@Adrians-MacBook-Pro.local>
2015-10-14 15:13:32 -0400

    added siskle to movie finder

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require './models/siskel.rb'
 
-get '/'do 
-	erb :'index.html'
+
+get '/' do 
+  siskel = Siskel.new("Lion King")
+	# erb :'index.html'
+	"The movie plot is: \n\t" "- #{siskel.plot}"
 end
+
